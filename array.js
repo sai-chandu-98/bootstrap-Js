04cdef5d8808687cc17f488ef6254f05c101a9ed
sai-chandu-98 <74167819+sai-chandu-98@users.noreply.github.com>
2021-07-02 10:45:21 +0530

    Add files via upload

---
+++ array.js
@@ -0,0 +1,49 @@
+// var num = [2,4,6,8,10]
+
+// function check(num)
+// {
+//     return num%2==0
+// }
+// var bool=num.every(check)
+
+// var num = [3,4,6,8,10]
+
+// var bool=num.every(element=>element%2==0)
+
+// console.log(bool);
+
+// num.forEach(e=>console.log(e))
+
+// var even = num.filter(ele=>ele%2==0)
+
+// console.log(even)
+
+// var bank ={
+//     accountno:12343455,
+//     ifsc: 'shd1234',
+//     branch:'hyd'
+
+// }
+
+// console.log(bank.accountno);
+
+// var num =25
+// function check()
+// {
+//     function sum()
+//     {
+//         console.log(num);
+//     }
+//      sum();
+// }
+// check();
+
+// var date = new Date()
+
+// console.log(date.toDateString())
+
+// console.log(date);
+
+localStorage.setItem('name', 'bob')
+
+localStorage.removeItem('name');
---
+++ index.js
@@ -0,0 +1,33 @@
+/*const a = 10;
+const b = 20
+console.log(a+b);
+console.log("hello world");*/
+
+//const nodemon = require("nodemon");
+
+
+// function test(num1,num2,num3)
+// {
+//     console.log("hello");
+//     console.log(num1+num2)
+// }
+// test(10,20);
+
+
+// var user={
+
+//     id:100,
+//     name:'chandu',
+//     accountnumber:12344534 
+// }
+
+// console.log(user)
+
+// function addNumber(number)
+// {
+//     return number+number
+// }
+
+// console.log(addNumber(3));
+
+
